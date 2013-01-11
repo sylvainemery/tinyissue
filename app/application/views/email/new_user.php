41d7bd5b4b00561f0f3f3c990b6441cdd8abd096
Sylvain Emery <github@emery.sc>
2013-01-11 16:49:22 +0000

    modified the New User email to give it some style

--- app/application/views/email/new_user.php
+++ app/application/views/email/new_user.php
@@ -1,4 +1,14 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Welcome to Tiny Issue</title>
+</head>
+<body>
+<div style="font-family: Helvetica, 'Lucida Grande', Arial, sans-serif;font-size: 12px;background: #fff;border-collapse: collapse;text-align: left;">
 <p>You have been set up with Tiny Issue at <?php echo URL::to(); ?></p>
 <p>
 You may log in with email <?php echo $email; ?> and password <?php echo $password; ?>
 </p>
+</div>
+</body>
+
